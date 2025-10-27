7ccc63249fb8897a064c4a6edea0227161013714
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-10-27 22:47:33 +0530

    login component added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from "./login/login";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = 'Yash, Learning Angular!';
   protected readonly what_to_become = 'A best Programmer';
+
 }
