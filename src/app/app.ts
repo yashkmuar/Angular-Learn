9d6efc585b8d7568f77cc4abaed88662786b5b2e
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-10-27 23:13:17 +0530

    sign up component added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from "./login/login";
+import { Signup } from "./signup/signup";
 
 @Component({
   selector: 'app-root',
-  imports: [Login],
+  imports: [Login, Signup],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
